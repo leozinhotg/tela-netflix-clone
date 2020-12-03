7797dba8cb08099d5d700e8f0c79e282a97e684a
Leandro <santos.leo2011@gmail.com>
2020-12-03 01:25:27 -0300

    Clone Netflix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clone da Tela do Netflix
+
+Fiz essa tela com a finalidade de aprendizado! :satisfied:
+
+ 
---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1100:{
+            items:5
+        },
+        1400:{
+            items:6
+        },
+        1500:{
+            items:7
+        },
+        1600:{
+            items:8
+        }
+    }
+})
